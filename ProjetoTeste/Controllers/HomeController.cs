b07101ceb3d42f75fa588675889d735f98ea172f
lucas466 <lfelix466@gmail.com>
2019-06-26 07:58:51 -0300

    Versao 1

---
+++ ProjetoTeste/Controllers/HomeController.cs
@@ -0,0 +1,228 @@
+﻿using Newtonsoft.Json.Linq;
+using ProjetoTeste.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoTeste.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //Variaveis que salvam o email a senha logados
+        static String Email, Senha;
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult BitcoinPrecos()
+        {
+            /*Verifica se foi possivel receber os dados da API*/
+            Usuario U = new Usuario();
+            float Preco = U.PegaApi();
+
+            if (Preco == 0)
+            {
+                ViewBag.Erro = "Infelizmente houve algum erro ao pegar a Api";
+                Response.Redirect("Index");
+            }
+            else
+            {
+                ViewBag.Preco  = Preco;
+            }
+
+            return View();
+
+        }
+
+        public ActionResult SobreBitcoin()
+        {
+            return View();
+        }
+
+        public ActionResult SobreNos()
+        {
+            return View();
+        }
+
+        public ActionResult EntrarConta()
+        {
+
+            if (Request.HttpMethod.Equals("POST"))
+            {
+                /*Verifica se o usuario ja fez login*/
+                try { 
+                Usuario U = new Usuario();
+                U.Email = Request.Form["Email"].ToString();
+                U.Senha = Request.Form["Senha"].ToString();
+
+                    if(ViewBag.Mensagem = U.EntrarUsuario().Equals("Deu certo")){
+                        Email = U.Email;
+                        Senha = U.Senha;
+                        return RedirectToAction("VerConta", "Home");
+                    }
+                }
+                catch 
+                {
+                    ViewBag.Mensagem = "Usuário e/ou senha inválido(s).";
+                }
+
+            }
+
+            return View();
+        }
+
+        public ActionResult CriarConta()
+        {
+
+            if (Request.HttpMethod.Equals("POST"))
+            {
+                Usuario U = new Usuario();
+                U.Email = Request.Form["Email"].ToString();
+                U.Nome = Request.Form["Nome"].ToString();
+                U.Telefone = Convert.ToInt32(Request.Form["Telefone"].ToString());
+                U.Cartao = Convert.ToInt32(Request.Form["Cartao"].ToString());
+                U.Senha = Request.Form["Senha1"].ToString();
+                U.Senha2 = Request.Form["Senha2"].ToString();
+
+                if (U.Telefone.ToString().Length >3)
+                {
+                    ViewBag.Userad = "Erro, use até 3 algarismo no campo telefone";
+
+                } else if (U.Cartao.ToString().Length > 3)
+                {
+                    ViewBag.Userad = "Erro, use até 3 algarismo no campo Cartao";
+
+                } else if (!U.Senha.Equals(U.Senha2))
+                {
+                    ViewBag.Userad = "Erro, os dois campos de senha deevem ser iguais";
+                }
+                else if (U.Senha.Length > 20 || U.Senha.Length > 20)
+                {
+                    ViewBag.Userad = "Erro, A senha pode conter no máximo 20 carateres";
+                }
+                else if (U.Nome.Length > 20 || U.Senha.Length > 20)
+                {
+                    ViewBag.Userad = "Erro, A nome pode conter no máximo 20 carateres";
+                }
+                else if (U.Email.Length > 20 || U.Senha.Length > 20)
+                {
+                    ViewBag.Userad = "Erro, O email pode conter no máximo 20 carateres";
+                }
+                else
+                {
+                    /*Verifica se o email ja existe*/
+                    String resultado;
+
+                    if (U.CriarUsuario())
+                    {
+                        resultado = "Conta cadastrada";
+                    }
+                    else
+                    {
+                        resultado = "Erro, email já existe";
+                    }
+                    ViewBag.Userad = resultado;
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult VerConta()
+        {
+            try
+            {
+                /*Testa se esta vazio*/
+            if (!Email.Equals("") || !Senha.Equals(""))
+            {
+                /*Recebe os valores do model e joga nas viewbags*/
+                Usuario U = new Usuario();
+                List<String> valores = new List<String>();
+                valores = U.MinhaConta(Email, Senha);
+
+                ViewBag.Nome = valores[0];
+                ViewBag.Email = valores[1];
+                ViewBag.Telefone = valores[2];
+                ViewBag.Cartao = valores[3];
+                ViewBag.Senha = valores[4];
+
+                if (valores[5].Equals(""))
+                {
+                    ViewBag.BitCoin = "0";
+                }
+                else
+                {
+                    ViewBag.BitCoin = valores[5];
+                }
+            }
+
+            }
+            catch
+            {
+                Response.Redirect("/Home/Index");
+            }
+
+
+            return View();
+        }
+
+        public ActionResult ConfirmacaoCompra(String ID)
+        {
+            try
+            {
+                if (ID.Equals("1") || ID.Equals("5") || ID.Equals("10") || ID.Equals("20"))
+                {
+                    Usuario U = new Usuario();
+                    int preco = Convert.ToInt32(U.PegaApi());  
+
+                    ViewBag.Numero = ID;
+                    ViewBag.Valor = preco;
+
+                    if (Request.HttpMethod.Equals("POST"))
+                    {
+                        try
+                        {
+                            U.Email = Request.Form["Email"].ToString();
+                            U.Senha = Request.Form["Senha"].ToString();
+                            Email = U.Email;
+                            Senha = U.Senha;
+                            U.Quantidade = Convert.ToInt32(Request.Form["quantidade"].ToString());
+
+                            if (U.Comprar(Email, Senha, U.Quantidade))
+                            {
+                                return RedirectToAction("VerConta", "Home");
+                            }
+                            else
+                            {
+                                ViewBag.Mensagem = "Usuário e/ou senha inválido(s).";
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            ViewBag.Mensagem = e;
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    Response.Redirect("/Home/BitcoinPrecos");
+                }
+
+            }
+            catch
+            {
+                Response.Redirect("/Home/BitCoinPrecos");
+            }
+            return View();
+        }
+    }
+}
---
+++ ProjetoTeste/Models/Usuario.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace ProjetoTeste.Models
+{
+    public class Usuario
+    {
+
+        public String Email { get; set; }
+        public String Nome { get; set; }
+        public Int32 Telefone { get; set; }
+        public Int32 Cartao { get; set; }
+        public String Senha { get; set; }
+        public String Senha2 { get; set; }
+        public String Carteira { get; set; }
+        public Int32 Quantidade { get; set; }
+
+        public Usuario() { }
+
+        public float PegaApi()
+        {
+            float Preco = 0;
+
+            try {
+
+            //Pega a api
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString("https://s3.amazonaws.com/data-production-walltime-info/production/dynamic/meta.json?now=1517922306634.319625.92");
+                JObject obj = JObject.Parse(json);
+                Object valor = (Object)obj["best_offer"];
+
+                //Transforma o resultado para  uma array
+                string valorArray = "[" + valor.ToString() + "]";
+                String valores;
+
+
+                //Pega o valor pela array
+                JArray parsedArray = JArray.Parse(valorArray);
+                foreach (JObject parsedObject in parsedArray.Children<JObject>())
+                {
+                    foreach (JProperty NomePassado in parsedObject.Properties())
+                    {
+                        string NomePropriedade = NomePassado.Name;
+                        if (NomePropriedade.Equals("xbt-brl"))
+                        {
+                            //Joga o valor para o viewbag mostrar na página
+                            string propertyValue = (string)NomePassado.Value;
+                            valores = propertyValue;
+
+                            /*Calcula o valor em real*/
+                            int porcentagem = Convert.ToInt32(valores.Substring(0, valores.IndexOf("/")));
+                            int valorJason = Convert.ToInt32(valores.Substring(valores.IndexOf("/") + 1));
+
+                            Preco = (100 * valorJason) / porcentagem;
+
+                        }
+                    }
+                }
+    
+            }
+            }
+            catch
+            {
+
+            }
+
+            return Preco;
+    }
+
+        public String EntrarUsuario()
+        {
+
+            String Resultado = "";
+
+            SqlConnection connection = new SqlConnection("yourDatabase");
+            connection.Open();
+
+            try
+            {
+
+                SqlCommand commando = new SqlCommand();
+                commando.Connection = connection;
+                commando.CommandText = "SELECT * FROM USUARIO WHERE EMAIL = @Email AND Senha = @Senha";
+                commando.Parameters.AddWithValue("@Email", Email);
+                commando.Parameters.AddWithValue("@Senha", Senha);
+
+                SqlDataReader reader = commando.ExecuteReader();
+
+               
+                if (reader.HasRows){
+
+                    Resultado = "Deu certo";
+
+                }
+                else
+                {
+                    Resultado = "Usuário e/ou senha inválido(s).";
+                }
+
+            }
+            catch
+            {
+                Resultado = "Usuário e/ou senha inválido(s).";
+            } 
+            finally
+            {
+                connection.Close();
+            }
+
+            return Resultado;
+
+        }
+
+        public Boolean CriarUsuario()
+        {
+            Boolean Resultado = false;
+            SqlConnection connection = new SqlConnection("yourDatabase");
+            connection.Open();
+            try
+            {
+                SqlCommand commando = new SqlCommand();
+                commando.Connection = connection;
+                commando.CommandText = "INSERT INTO USUARIO (Email, Nome, Telefone, Cartao, Senha) VALUES (@Email, @Nome, @Telefone, @Cartao, @Senha)";
+                commando.Parameters.AddWithValue("@Email", Email);
+                commando.Parameters.AddWithValue("@Nome", Nome);
+                commando.Parameters.AddWithValue("@Telefone", Telefone);
+                commando.Parameters.AddWithValue("@Cartao", Cartao);
+                commando.Parameters.AddWithValue("@Senha", Senha);
+
+                if (commando.ExecuteNonQuery() > 0)
+                {
+                    Resultado = true;
+                }
+
+            }
+            catch 
+            {
+                Resultado = false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+            return Resultado;
+
+        }
+
+        public List<String> MinhaConta(String Email, String Senha)
+        {
+            List<String> valores = new List<String>();
+
+            SqlConnection connection = new SqlConnection("yourDatabase");
+            connection.Open();
+            try
+            {
+                SqlCommand commando = new SqlCommand();
+                commando.Connection = connection;
+                commando.CommandText = "SELECT * FROM USUARIO WHERE Email = @Email AND Senha = @Senha";
+                commando.Parameters.AddWithValue("@Email", Email);
+                commando.Parameters.AddWithValue("@Senha", Senha);
+
+                SqlDataReader Leitor = commando.ExecuteReader();
+
+                while (Leitor.Read())
+                {
+                    Usuario U = new Usuario();
+                    valores.Add(U.Nome = Leitor["Nome"].ToString());
+                    valores.Add(U.Email = Leitor["Email"].ToString());
+                    U.Telefone = Convert.ToInt32(Leitor["Telefone"]);
+                    valores.Add(Convert.ToString(U.Telefone));
+                    U.Cartao = Convert.ToInt32(Leitor["Cartao"]);
+                    valores.Add(Convert.ToString(U.Cartao));
+                    valores.Add(U.Senha = Leitor["Senha"].ToString());
+                    valores.Add(U.Carteira = Leitor["Carteira"].ToString());
+
+                }
+
+                return valores;
+            }
+            catch
+            {
+
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+            };
+
+        }
+
+        public Boolean Comprar(String Email, String Senha, Int32 Valor)
+        {
+            Boolean Resultado = false;
+
+            if (EntrarUsuario().Equals("Deu certo"))
+            {
+                List<String> valor = new List<String>();
+                valor = MinhaConta(Email, Senha);
+
+                int valorAtual, ValorResutado;
+
+                if (valor[5].Equals(""))
+                {
+                    valorAtual = 0;
+                }
+                else
+                {
+                    valorAtual = Convert.ToInt32(valor[5]);
+                }
+
+                ValorResutado = valorAtual + Valor;
+
+                SqlConnection connection = new SqlConnection("yourDatabase");
+                connection.Open();
+
+                SqlCommand commando = new SqlCommand();
+                commando.Connection = connection;
+                commando.CommandText = "UPDATE USUARIO SET Carteira = @Carteira WHERE Email = @Email";
+                commando.Parameters.AddWithValue("@Carteira", ValorResutado);
+                commando.Parameters.AddWithValue("@Email", Email);
+
+                if (commando.ExecuteNonQuery() > 0)
+                {
+                    Resultado = true;
+                }
+
+               
+            }
+            return Resultado;
+        }
+         
+        
+    }
+}
